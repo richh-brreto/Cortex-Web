ff8be581a1a46d2c51d59851010c8fedcfa3803c
WillianbsilvaDev <willian.dsilva@sptech.school>
2025-08-24 16:19:55 -0300

    BD logico e fisico adicionado

--- web-data-viz-main/src/database/script-tabelas.sql
+++ web-data-viz-main/src/database/script-tabelas.sql
@@ -1,62 +1,49 @@
--- Arquivo de apoio, caso você queira criar tabelas como as aqui criadas para a API funcionar.
--- Você precisa executar os comandos no banco de dados para criar as tabelas,
--- ter este arquivo aqui não significa que a tabela em seu BD estará como abaixo!
+create database CORTEX;
+use CORTEX;
 
-/*
-comandos para mysql server
-*/
-
-CREATE DATABASE aquatech;
-
-USE aquatech;
+CREATE TABLE endereco (
+    idendereco INT PRIMARY KEY AUTO_INCREMENT,
+    logradouro VARCHAR(100),
+    numero VARCHAR(10),
+    complemento VARCHAR(50),
+    bairro VARCHAR(50),
+    cidade VARCHAR(50),
+    estado CHAR,
+    cep VARCHAR(9)
+);
 
 CREATE TABLE empresa (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	razao_social VARCHAR(50),
-	cnpj CHAR(14),
-	codigo_ativacao VARCHAR(50)
+    idempresa INT PRIMARY KEY AUTO_INCREMENT,
+    nome VARCHAR(100),
+    CNPJ VARCHAR(18),
+    telefone VARCHAR(20),
+    ativo TINYINT,
+    fkendereco INT,
+    FOREIGN KEY (fkendereco) REFERENCES endereco(idendereco)
 );
 
 CREATE TABLE usuario (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	nome VARCHAR(50),
-	email VARCHAR(50),
-	senha VARCHAR(50),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+    idusuario INT PRIMARY KEY AUTO_INCREMENT,
+    idempresa INT,
+    email VARCHAR(100),
+    senha VARCHAR(20),
+    administrador BOOLEAN DEFAULT TRUE,
+    ativo BOOLEAN DEFAULT TRUE,
+    FOREIGN KEY (idempresa) REFERENCES empresa(idempresa)
 );
 
-CREATE TABLE aviso (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	titulo VARCHAR(100),
-	descricao VARCHAR(150),
-	fk_usuario INT,
-	FOREIGN KEY (fk_usuario) REFERENCES usuario(id)
+CREATE TABLE datacenter (
+    iddatacenter INT PRIMARY KEY AUTO_INCREMENT,
+    idendereco INT,
+    identificacao VARCHAR(80),
+    FOREIGN KEY (idendereco) REFERENCES endereco(idendereco)
 );
 
-create table aquario (
-/* em nossa regra de negócio, um aquario tem apenas um sensor */
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	descricao VARCHAR(300),
-	fk_empresa INT,
-	FOREIGN KEY (fk_empresa) REFERENCES empresa(id)
+CREATE TABLE alerta (
+    idAlerta INT PRIMARY KEY AUTO_INCREMENT,
+    duracao INT,
+    limite INT,
+    inicio DATETIME,
+    datacenter_iddatacenter INT,
+    FOREIGN KEY (datacenter_iddatacenter) REFERENCES datacenter(iddatacenter)
 );
-
-/* esta tabela deve estar de acordo com o que está em INSERT de sua API do arduino - dat-acqu-ino */
-
-create table medida (
-	id INT PRIMARY KEY AUTO_INCREMENT,
-	dht11_umidade DECIMAL,
-	dht11_temperatura DECIMAL,
-	luminosidade DECIMAL,
-	lm35_temperatura DECIMAL,
-	chave TINYINT,
-	momento DATETIME,
-	fk_aquario INT,
-	FOREIGN KEY (fk_aquario) REFERENCES aquario(id)
-);
-
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 1', 'ED145B');
-insert into empresa (razao_social, codigo_ativacao) values ('Empresa 2', 'A1B2C3');
-insert into aquario (descricao, fk_empresa) values ('Aquário de Estrela-do-mar', 1);
-insert into aquario (descricao, fk_empresa) values ('Aquário de Peixe-dourado', 2);
