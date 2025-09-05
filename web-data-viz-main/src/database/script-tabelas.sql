94244839c3a1568bbd92bfab6dc7ea3410b77175
giuliasantiagos <giulia.silva@sptech.school>
2025-09-05 11:17:31 -0300

    alterações no BD (script e DER)

--- web-data-viz-main/src/database/script-tabelas.sql
+++ web-data-viz-main/src/database/script-tabelas.sql
@@ -1,58 +1,42 @@
-drop database cortex;
+-- drop database cortex;
 create database cortex;
 use cortex;
 
 CREATE TABLE Empresa (
     idEmpresa INT PRIMARY KEY AUTO_INCREMENT,
-    Nome VARCHAR(100),
-    CNPJ VARCHAR(18) UNIQUE,
-    ativo TINYINT, 
-    Nome_responsavel VARCHAR(100),
-    Telefone_responsavel VARCHAR(45)
-);
-
-CREATE TABLE Documento (
-    idDocumento INT PRIMARY KEY AUTO_INCREMENT,
-    FK_empresa INT,
-    nome_documento VARCHAR(45),
-    formato_documento VARCHAR(45),
-    documento MEDIUMBLOB,
-    FOREIGN KEY (FK_empresa) REFERENCES Empresa(idEmpresa)
+    Nome VARCHAR(100) NOT NULL,
+    CNPJ VARCHAR(18) UNIQUE NOT NULL,
+    ativo TINYINT NOT NULL, 
+    Nome_responsavel VARCHAR(100) NOT NULL,
+    Telefone_responsavel VARCHAR(11) UNIQUE NOT NULL
 );
 
 CREATE TABLE Usuario (
     idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     FK_empresa INT,
-    Nome VARCHAR(100),
-    Email VARCHAR(100),
-    Senha VARCHAR(20),
-    Administrador BOOLEAN DEFAULT TRUE,
-    Ativo BOOLEAN DEFAULT TRUE,
+    Nome VARCHAR(100) NOT NULL,
+    Email VARCHAR(100) UNIQUE NOT NULL,
+    Senha VARCHAR(20) NOT NULL,
+    Administrador BOOLEAN DEFAULT TRUE NOT NULL,
+    Ativo BOOLEAN DEFAULT TRUE NOT NULL,
     FOREIGN KEY (FK_empresa) REFERENCES Empresa(idEmpresa)
 );
 
 CREATE TABLE Datacenter (
     idDatacenter INT PRIMARY KEY AUTO_INCREMENT,
     FK_empresa INT,
-    Nome_datacenter VARCHAR(80),
-    CEP VARCHAR(9),
-    Complemento VARCHAR(100),
-    Numero VARCHAR(10),
+    Nome_datacenter VARCHAR(80) NOT NULL,
+    CEP VARCHAR(9) NOT NULL,
+    Complemento VARCHAR(100) NOT NULL,
+    Numero VARCHAR(10) NOT NULL,
     FOREIGN KEY (FK_empresa) REFERENCES Empresa(idEmpresa)
 );
-
-CREATE TABLE Rack (
-	idRack INT PRIMARY KEY AUTO_INCREMENT,
-	FK_datacenter INT,
-    Nome_rack VARCHAR(45),
-	FOREIGN KEY (FK_datacenter) references Datacenter(idDatacenter)
-    );
 
 CREATE TABLE Servidor (
     idServidor INT PRIMARY KEY AUTO_INCREMENT,
 	FK_rack INT,
-    Identificacao VARCHAR(80),
-    FOREIGN KEY (FK_rack) REFERENCES Rack(idRack)
+    Identificacao VARCHAR(80) UNIQUE NOT NULL,
+    FOREIGN KEY (FK_rack) references Datacenter(idDatacenter)
 );
     
 CREATE TABLE ServidorUsuario (
@@ -63,25 +47,12 @@ CREATE TABLE ServidorUsuario (
     PRIMARY KEY (FK_servidor, FK_usuario)
 );
 
-CREATE TABLE EspecificaçãoHardware (
-    idEspecificacaoHardware INT PRIMARY KEY AUTO_INCREMENT,
-    Modelo_cpu VARCHAR(45),
-    Qtd_cpu INT NOT NULL,
-    Qtd_disco INT NOT NULL,
-    Qtd_ram INT NOT NULL,
-    FK_servidor INT,
-    FOREIGN KEY (FK_servidor) REFERENCES Servidor(idServidor)
-);
-
 CREATE TABLE Processo (
     idProcesso INT AUTO_INCREMENT,
     FK_servidor INT,
-    Nome_componente VARCHAR(45),
-    Nome_processo VARCHAR(45),
-    Metrica_decimal DECIMAL (10, 2),
-    Metrica_string VARCHAR(45),
-    Metrica_percent DECIMAL (10, 2),
-    Data_hora DATETIME,
+    Nome_processo VARCHAR(45)NOT NULL,
+    Metrica_percent DECIMAL (10, 2) NOT NULL,
+    Data_hora DATETIME NOT NULL,
     PRIMARY KEY (idProcesso, FK_servidor),
     FOREIGN KEY (FK_servidor) REFERENCES Servidor(idServidor)
 );
@@ -89,21 +60,10 @@ CREATE TABLE Processo (
 CREATE TABLE MetricaAlerta (
     idAlerta INT AUTO_INCREMENT,
     FK_processo INT,
-    Maximo DECIMAL(10, 2),
-    Minimo DECIMAL(10, 2),
-    Inicio DATETIME,
-    Fim DATETIME,
+    Maximo DECIMAL(10, 2) NOT NULL,
+    Minimo DECIMAL(10, 2) NOT NULL,
+    Inicio DATETIME NOT NULL,
+    Fim DATETIME NOT NULL,
     PRIMARY KEY (idAlerta, FK_processo),
     FOREIGN KEY (FK_processo) REFERENCES Processo(idProcesso)
-);
-
-CREATE TABLE MetricasDisponiveis (
-    FK_processo INT,
-    FK_servidor INT,
-    Nome_opcao VARCHAR(45),
-    Nome_componente VARCHAR(45),
-    Nome_metrica VARCHAR(45),
-    PRIMARY KEY (FK_processo, FK_servidor),
-    FOREIGN KEY (FK_processo) REFERENCES Processo(idProcesso),
-    FOREIGN KEY (FK_servidor) REFERENCES Servidor(idServidor)
 );
