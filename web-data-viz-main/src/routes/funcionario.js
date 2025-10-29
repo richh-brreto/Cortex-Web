4bd5ad89d3da6fa4e5df7453e888cc33ff3fee2d
richh-brreto <richard.barreto@sptech.school>
2025-10-29 00:40:42 -0300

    fix(web-data-viz-main): Fluxo de imagem/atualizar e nome por sessionStorage - rota corrigida para pegar o nome do caminho no banco - model corrigido para pegar o campo foto do banco - campo foto no pop-up do HTML gerenciamento_de_funcionario - alteração na função de gerenciamento_de_funcionario para criar formData - colocando foto no sessionStorage do login

--- web-data-viz-main/src/routes/funcionario.js
+++ web-data-viz-main/src/routes/funcionario.js
@@ -1,11 +1,12 @@
 var express = require("express");
+var upload = require("../config/configUpload");
 var router = express.Router();
 
 var funcionarioController = require("../controllers/funcionarioController");
 
 router.get("/listar/:fkEmpresa", funcionarioController.listar);
 router.post("/cadastrar/:fkEmpresa", funcionarioController.cadastrar);
-router.put("/atualizar/:id", funcionarioController.atualizar);
+router.put("/atualizar/:id", upload.single("foto"), funcionarioController.atualizar);
 router.delete("/deletar/:id", funcionarioController.deletar);
 
 module.exports = router;
